e8ac6f330fcfc50e6b45b0bc606cf32f729865e7
babucarr32 <baboucarry@gmail.com>
2022-08-26 00:07:01 +0100

    Adding features

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
+from os import link
+from urllib import request
+
+
 try:
     from bs4 import BeautifulSoup
+    import requests
     from googlesearch import search
 except Exception as e:
     print('Module not found: Make sure you have BeautifulSoup and google installed')
 
 def Harvester():
     query = input('Enter query: ')
-    for q in search(query, tld="co.in", num=50, stop=50, pause=2):
-        print(j)
+    targets = []
+    targetLinks = []
+    print("Searching...")
+    try:
+        for q in search(query, tld="co.in", num=50, stop=50, pause=3): # search the web 
+            targets.append(q)
+            # print(q)
+        
+        for target in targets: # attacking target websites
+            # print(target)
+            htmlText = requests.get(target).text
+            mySoup = BeautifulSoup(htmlText, 'lxml')
+            links =  mySoup.find_all("a")
+            for link in links:
+                x = link['href']
+                if 'geeksforgeek' in x:
+                    print(x)
+                    # return targetLinks.append(link['href'])
+            # # print(mySoup.prettify())
+            # break
+    except Exception as e:
+        print(e)
+        pass
+    return targetLinks
+if __name__ ==  '__main__':
+    Harvester()
