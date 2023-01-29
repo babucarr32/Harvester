2083e8e3e9f142854241bf07381163f6e2402735
babucarr32 <baboucarry@gmail.com>
2023-01-29 22:39:11 +0000

    imported regex

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from os import link
 from urllib import request
+import re
 
 
 try:
@@ -15,19 +16,23 @@ def Harvester():
     targetLinks = []
     print("Searching...")
     try:
-        for q in search(query, tld="co.in", num=50, stop=50, pause=3): # search the web 
+        for q in search(query, tld="co.in", num=10, stop=10, pause=3): # search the web 
             targets.append(q)
             # print(q)
         
+        print(targets)
         for target in targets: # attacking target websites
             # print(target)
             htmlText = requests.get(target).text
             mySoup = BeautifulSoup(htmlText, 'lxml')
             links =  mySoup.find_all("a")
             for link in links:
-                x = link['href']
-                if 'geeksforgeek' in x:
-                    print(x)
+                try:
+                    x = link['href']
+                    if 'taf' in x:
+                        print(x)
+                except Exception as e:
+                    pass
                     # return targetLinks.append(link['href'])
             # # print(mySoup.prettify())
             # break
