4fa97481815aebe49b2bff8598151a694d9d0376
babucarr32 <baboucarry@gmail.com>
2023-01-29 23:45:49 +0000

    Added readme file

---
+++ README.md
@@ -0,0 +1,4 @@
+# Harvester
+This tool allows you to harvest informations likes emails, urls, links, etc.
+By using the command v, it get violent and fetches more informations.
+The author assumes no responsibility or liability for any content of this script.
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from os import link
 from urllib import request
 import re
+import sys
 
 
 try:
@@ -13,29 +14,46 @@ except Exception as e:
 def Harvester():
     query = input('Enter query: ')
     targets = []
+    secondTarget = []
+    validTargets = []
     targetLinks = []
-    print("Searching...")
+    print("Searching, this might take time...")
     try:
         for q in search(query, tld="co.in", num=10, stop=10, pause=3): # search the web 
             targets.append(q)
             # print(q)
-        
-        print(targets)
+
         for target in targets: # attacking target websites
-            # print(target)
             htmlText = requests.get(target).text
             mySoup = BeautifulSoup(htmlText, 'lxml')
             links =  mySoup.find_all("a")
             for link in links:
                 try:
                     x = link['href']
-                    if 'taf' in x:
-                        print(x)
+                    if query in x:
+                        secondTarget.append(x)
+                        validTargets.append(x)
                 except Exception as e:
                     pass
-                    # return targetLinks.append(link['href'])
-            # # print(mySoup.prettify())
-            # break
+        
+        if "v" in sys.argv:
+            for target in secondTarget:
+                try:
+                    htmlText = requests.get(target).text
+                    mySoup = BeautifulSoup(htmlText, 'lxml')
+                    links =  mySoup.find_all("a")
+                    for link in links:
+                        try:
+                            x = link['href']
+                            if query in x:
+                                validTargets.append(x)
+                        except Exception as e:
+                            pass
+                except Exception:
+                    pass
+            print(validTargets)
+        else:
+            print(validTargets)
     except Exception as e:
         print(e)
         pass
